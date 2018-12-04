21dc583f89794ca79f4be4bf1e26606dd75d644a
Anton Fedchin <anightik@gmail.com>
2018-12-04 16:24:41 +0300

    MYS-14767: added LegalHoldsService and LegalHold.

---
+++ CSharpSampleApp/Entities/User/LegalHold.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace CSharpSampleApp.Entities
+{
+    [DataContract(Namespace = "")]
+    public class LegalHold
+    {
+        [DataMember(EmitDefaultValue = false, Order = 0)]
+        public Guid? LegalHoldId { get; set; }
+
+        [DataMember(EmitDefaultValue = false, Order = 1)]
+        public User User { get; set; }
+
+        [DataMember(EmitDefaultValue = false, Order = 2)]
+        public DateTime? DateExpiredUtc { get; set; }
+
+        [DataMember(EmitDefaultValue = false, Order = 3)]
+        public User RequestedByUser { get; set; }
+
+        [DataMember(EmitDefaultValue = false, Order = 4)]
+        public DateTime? DateCreatedUtc { get; set; }
+    }
+}
